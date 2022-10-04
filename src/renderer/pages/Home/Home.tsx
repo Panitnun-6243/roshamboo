fd48911d7e8f90934e7233ad4cd64802b4c723d0
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-04 16:58:51 +0700

    Draft Homepage

--- src/renderer/pages/Home/Home.tsx
+++ src/renderer/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
 import './home.css';
 
-export default function Homepage() {
-  return <div>Homepage</div>;
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="game-title-box">
+        <div className="game-title">Roshamboo!</div>
+      </div>
+    </div>
+  );
 }
